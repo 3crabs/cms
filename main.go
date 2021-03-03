9048754aa77b89722e9308074524ee7492777d17
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2021-03-03 22:39:20 +0700

    add / /health /posts

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, _ *http.Request) {
+	_, _ = w.Write([]byte("Welcome to the cms from 3crabs!"))
+}
+
+func GetPosts(w http.ResponseWriter, _ *http.Request) {
+	_, _ = w.Write([]byte("hello"))
+}
 
 func main() {
-	fmt.Println("Hello, World!!")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Heartbeat("/health"))
+	r.Get("/", Index)
+	r.Get("/posts", GetPosts)
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		panic(err)
+	}
 }
